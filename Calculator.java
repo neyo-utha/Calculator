eb8dad7494670c09131927de0d51d1ee6f3686a5
Neyo Utha <117210225+neyo-utha@users.noreply.github.com>
2023-03-24 10:58:38 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,6 @@ public class Calculator
     
     public void add(double num1, double num2)
     {
-        
         double sum = num1 + num2;
         System.out.println("The sum is " + sum );
     }
